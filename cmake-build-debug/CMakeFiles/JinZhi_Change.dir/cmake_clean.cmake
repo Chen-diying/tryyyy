36660e8c8d24c86532c1312419dc920215624875
Geng_long <2229793224@qq.com>
2019-10-13 10:35:26 +0800

    This is pushed by Geng-long

---
+++ cmake-build-debug/CMakeFiles/JinZhi_Change.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/JinZhi_Change.dir/main.cpp.obj"
+  "JinZhi_Change.pdb"
+  "JinZhi_Change.exe"
+  "JinZhi_Change.exe.manifest"
+  "libJinZhi_Change.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/JinZhi_Change.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
